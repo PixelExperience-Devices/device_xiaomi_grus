1a0032a0ff013d3e969036e9b42d32ec7d53a559
pengus77 <ivan@cerrato.biz>
2020-05-07 14:45:49 +0200

    grus: fod: bring in the poly interpolation for alpha/brightness ratio

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -66,6 +66,22 @@ namespace inscreen {
 namespace V1_0 {
 namespace implementation {
 
+/*  
+    map the polynomial function here based on the discovered points
+
+    ALPHA = 1.0 | BRIGHTNESS = 0
+    ALPHA = 0.7 | BRIGHTNESS = 150
+    ALPHA = 0.5 | BRIGHTNESS = 475
+    ALPHA = 0.3 | BRIGHTNESS = 950
+    ALPHA = 0.0 | BRIGHTNESS = 2047
+*/
+
+float p1 = 7.747 * pow(10, -8);
+float p2 = -0.0004924;
+float p3 = 0.6545;
+float p4 = 58.82;
+float q1 = 58.82;
+
 FingerprintInscreen::FingerprintInscreen() {
     xiaomiFingerprintService = IXiaomiFingerprint::getService();
 }
@@ -132,11 +148,7 @@ Return<int32_t> FingerprintInscreen::getDimAmount(int32_t /*brightness*/) {
     int realBrightness = get(BRIGHTNESS_PATH, 0);
     float alpha;
 
-    if (realBrightness > 500) {
-        alpha = 1.0 - pow(realBrightness / 2047.0 * 430.0 / 600.0, 0.455);
-    } else {
-        alpha = 1.0 - pow(realBrightness / 1680.0, 0.455);
-    }
+    alpha = (p1 * pow(realBrightness, 3) + p2 * pow(realBrightness, 2) + p3 * realBrightness + p4) / (realBrightness + q1);
 
     return 255 * alpha;
 }
