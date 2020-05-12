399e191a807ceb82fe3d3c10ba79d9359556e5e2
pengus77 <ivan@cerrato.biz>
2020-05-12 14:26:23 +0200

    camera: don't build snap as it's included in the rom now

--- device.mk
+++ device.mk
@@ -167,7 +167,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # CNE
