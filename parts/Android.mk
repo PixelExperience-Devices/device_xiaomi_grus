de51a74392d6f22141b484325f68a4d1427f68c0
Luís Ferreira <contact@lsferreira.net>
2020-07-04 16:08:57 +0100

    grus: parts: add OpenXiaomiParts to the local overwritten packages
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- parts/Android.mk
+++ parts/Android.mk
@@ -13,6 +13,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := XiaomiParts
+LOCAL_OVERRIDES_PACKAGES := OpenXiaomiParts
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
